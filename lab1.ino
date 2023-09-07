8aa065016ae098d4334da8588b9e068fdab0393b
Prasit  Krongmaroeng <90641470+PrasitIsK@users.noreply.github.com>
2023-09-07 14:40:54 +0700

    LAB1 Of IoT

---
+++ lab1.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D0,HIGH);
+  digitalWrite(D1,HIGH);
+  digitalWrite(D2,HIGH);
+  delay(1000);
+  digitalWrite(D0,LOW);
+  digitalWrite(D1,LOW);
+  digitalWrite(D2,LOW);
+  delay(1000);
+}
